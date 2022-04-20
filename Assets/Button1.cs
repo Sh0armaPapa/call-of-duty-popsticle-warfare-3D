b5a22ad251154d5f152fb8e3f0c22d01c0e83c5c
Jeffrey smit <34200@ma-web.nl>
2022-04-20 10:58:31 +0200

    neiuw

---
+++ Assets/Button1.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button1 : MonoBehaviour
+{
+    public void DeleteImage()
+    {
+        Destroy(gameObject);
+    }
+}
